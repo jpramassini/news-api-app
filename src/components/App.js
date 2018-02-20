c52c21f70f6c02a277c65dfe177261ed057df8e6
JP Ramassini <jpramassini@gmail.com>
2018-02-19 20:55:05 -0500

    Commit before Heroku deploy.

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import {getTopArticles} from '../actions';
 import ArticlesList from '../containers/articles-list';
 import Navbar from './NavBar';
 import LandingPage from './LandingPage';
-import Fade from 'react-reveal/Fade'
+import Fade from 'react-reveal/Fade';
 
 class App extends Component {
   render() {
